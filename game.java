6daf43c8fe4a8f41d46acfdd0f9d83bbb1215526
thewarnold <ld99alt@gmail.com>
2026-01-28 09:32:37 -0600

    started foundation of main

--- game.java
+++ game.java
@@ -1,3 +1,15 @@
-public class game {
-    
+import java.util.*;
+import java.io.*;
+public class game
+{
+    public static void main(String[] args)
+    {
+        ArrayList<ArrayList<card>> cards = new ArrayList<>();
+        Stack<Integer> cardlist = new Stack<>();
+        // Adds numbers 1 to 52 in the stack. Card types can be determined in whatever way, following suit of 13s.
+        for(int x=0;x<52;x++)
+        {
+            cardlist.add(x);
+        }
+    }
 }
