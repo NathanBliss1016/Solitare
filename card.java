1822bfc05c5464948df0a69a6ca0af1d5c5c9a8b
NathanBliss1016 <NathanBliss2007@gmail.com>
2026-01-28 11:36:26 -0600

    HOW WORK

--- card.java
+++ card.java
@@ -1,3 +1,24 @@
-public class card {
-    
+import java.util.*;
+import java.io.*;
+
+public class card 
+{
+    private String mytype;
+    private int mynum;
+
+    public card(String type, int num)
+    {
+        mytype = type;
+        mynum = num;
+    }
+
+    public String gettype()
+    {
+        return mytype;
+    }
+
+    public int getnum()
+    {
+        return mynum; 
+    }
 }
--- game.java
+++ game.java
@@ -1,3 +1,33 @@
-public class game {
-    
+import java.util.*;
+
+public class game 
+{
+    public static void main(String[] args)
+    {
+        Stack<card> deck = new Stack<card>();
+        String[] suits = new String[4];
+        suits[0] = "Hearts";
+        suits[1] = "Diamonds";
+        suits[2] = "Spades";
+        suits[3] = "Clubs";
+        for (int lcv = 0;lcv < 52;lcv ++)
+        {
+            int temp = (int)((Math.random() * 13) + 1);
+            String temp2 = suits[(int)(Math.random() * 4)];
+            card x = new card(temp2,temp);
+            while (deck.contains(x))
+            {
+                temp = (int)((Math.random() * 13) + 1);
+                temp2 = suits[(int)(Math.random() * 4)];
+                x = new card(temp2,temp);
+            }
+            deck.push(x);
+        }
+
+        while (!deck.empty())
+        {
+            card x = deck.pop();
+            System.out.println(x.gettype() + x.getnum());
+        }
+    }
 }
